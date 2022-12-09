4bc491897dd70f6328b37228790b763365cbe6ce
TaeYeongKwak <abgudv6403@naver.com>
2022-12-09 16:00:34 +0900

    me clientIdx parsing

--- src/main/java/com/thing/gateway/config/filter/AuthenticationFilter.java
+++ src/main/java/com/thing/gateway/config/filter/AuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.thing.gateway.config.filter;
 
 import com.thing.gateway.config.jwt.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -13,7 +14,10 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+
 @RefreshScope // 설정 변경시 서버를 재실행
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFilter implements GatewayFilter {
@@ -29,7 +33,8 @@ public class AuthenticationFilter implements GatewayFilter {
             String token = getAuthHeaderValue(request);
 
             if (StringUtils.hasText(token) && jwtTokenUtils.validateToken(token)){
-                this.addRequestUserInfoHeader(exchange, token);
+                String clientIdx = addRequestUserInfoHeader(exchange, token);
+                exchange = rewritePathMe(exchange, clientIdx);
             }
         }
 
@@ -44,12 +49,21 @@ public class AuthenticationFilter implements GatewayFilter {
         return null;
     }
 
-    private void addRequestUserInfoHeader(ServerWebExchange exchange, String token){
+    private String addRequestUserInfoHeader(ServerWebExchange exchange, String token){
         Map<String, String> userInfo = jwtTokenUtils.getUserInfo(token);
         exchange.getRequest().mutate()
                 .header("clientIdx", userInfo.get("clientIdx"))
                 .header("role", userInfo.get("role"))
                 .build();
+        return userInfo.get("clientIdx");
+    }
+
+    private ServerWebExchange rewritePathMe(ServerWebExchange exchange, String clientIdx){
+        String path = exchange.getRequest().getURI().getRawPath();
+        String newPath = path.replaceAll("me", clientIdx);
+        ServerHttpRequest request = exchange.getRequest().mutate().path(newPath).build();
+        exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, request.getURI());
+        return exchange.mutate().request(request).build();
     }
 
 }
