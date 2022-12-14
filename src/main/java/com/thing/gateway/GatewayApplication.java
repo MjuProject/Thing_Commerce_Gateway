595c80a46bd579710634f95f4f7d1c71819b7371
5kso3 <5kso3@naver.com>
2022-12-14 22:18:43 +0900

    build

--- src/main/java/com/thing/gateway/GatewayApplication.java
+++ src/main/java/com/thing/gateway/GatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
